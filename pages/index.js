5d2981077dd340716f9c4c96d5c9efa3a12c8d4a
David Jurelius <davan839@gmail.com>
2018-08-21 13:04:34 +0200

    Put Agency logic in index to allow for next.js SSR available for pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
 import Link from 'next/link'
 import Layout from '../components/Layout.js'
 import Hero from '../components/Hero.js'
-import Agencies from '../components/Agencies.js'
 import Information from '../components/Information.js'
+import {loadDB} from './../lib/db.js'
 
-const Index = () => 
-  <Layout>
-    <Hero/>
-    <Agencies/>
-    <Information/> 
-  </Layout>
-
-export default Index
+export default class Index extends React.Component {
+  static async getInitialProps () {
+    let db = await loadDB();
+    let result = await new Promise((resolve, reject) => {
+      db.firestore()
+        .collection('agencies')
+        .limit(5)
+        .onSnapshot(snapshot => {
+          let newState = {
+            agencies: []
+          };
+          snapshot.forEach(function(doc) {
+            newState.agencies.push(Object.assign({
+              id: doc.id
+            }, doc.data()))
+          });
+          resolve(newState)
+        }, error => {
+          reject([])
+        }
+      )
+    })
+    return result
+  }
+  render () {
+    const agencies = this.props.agencies
+    return <Layout>
+      <Hero/>
+      <div>{agencies.length} agencies</div>
+      <ul>
+        {agencies.map(agency => <li key={agency.id}>{agency.name} ranked {agency.ranking}</li>)}
+      </ul>
+      <Information/> 
+    </Layout>
+  }
+}
