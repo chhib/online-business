7ee872f606d9cf3dcbe5bef82e10f03a6fdd0434
David Jurelius <davan839@gmail.com>
2018-08-21 14:35:09 +0200

    Fetching data with componentDidMount() (not SSR!!)

--- components/Agencies.js
+++ components/Agencies.js
@@ -1,35 +1,26 @@
-export default () =>
-  <div id="agencies">
-    <h2>Most trusted agencies in Stockholm</h2>
-    <p>We the brand asked for offers for our Sample Real Project and had an extra round of design fixes - that's our way of ranking these agencies.</p>
-    <ul>
-      <li>
-        <h3>DevTips Agency</h3>
-        <p>Very high quality agency. Expensive, but you can certainly trust them. Proactive and supreme. <a>Read full review.</a></p>
-        <dl>
-          <dt>Hourly rate:</dt>
-          <dd>5,000 $</dd>
-          <dt>Response time:</dt>
-          <dd>Short</dd>
-          <dt>Reviews:</dt>
-          <dd>6 / 5</dd>
-          <dt>Quality:</dt>
-          <dd>High! Very!</dd>
-        </dl>
-      </li>
-      <li>
-        <h3>ShopetiShopEcomExperts</h3>
-        <p>When you need something truly quick - and especially dirty - go for these guys.  <a>Read full review.</a></p>
-        <dl>
-          <dt>Hourly rate:</dt>
-          <dd>50 $</dd>
-          <dt>Response time:</dt>
-          <dd>5 ms</dd>
-          <dt>Reviews:</dt>
-          <dd>2 / 5</dd>
-          <dt>Quality:</dt>
-          <dd>No.</dd>
-        </dl>
-      </li>
-    </ul>
-  </div>
+import 'isomorphic-fetch'
+
+export default class extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      agencies: []
+    }
+  }
+  async componentDidMount() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const agencies = await response.json()
+    this.setState({agencies: agencies})
+  }
+  render() {
+    return <div id="agencies">
+      <h2>Most trusted agencies in Stockholm</h2>
+      <p>We the brand asked for offers for our Sample Real Project and had an extra round of design fixes - that's our way of ranking these agencies.</p>
+      {(this.state.agencies && this.state.agencies.length > 0) ? 
+        <ul>
+          {this.state.agencies.map(agency => <li><h3>{agency.title}</h3></li>)}
+        </ul> : <p><strong>Have nothing!!</strong></p>}
+        <hr/>
+    </div>
+    }
+  }
